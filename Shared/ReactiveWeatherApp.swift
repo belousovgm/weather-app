e3331eeb9699adbf36b1715d9a3b859c4ca595eb
Grigory Belousov <gbelousov@icerockdev.com>
2022-08-31 21:56:29 +0400

    Initial Commit

---
+++ Shared/ReactiveWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactiveWeatherApp.swift
+//  Shared
+//
+//  Created by Grigory Belousov on 31.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ReactiveWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
